a387c449cb4f4f16d39d865d12b5c8a53a43cc53
jkchao <jkchaom@gmail.com>
2017-04-26 05:57:46 +0800

    修改格式

--- src/components/emoji.vue
+++ src/components/emoji.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="emoji">
     <ul class="emoji-controller">
-      <li v-for="(pannel,index) in pannels" @click="changeActive(index)"
-      :class="{'active': index === activeIndex}">{{ pannel }}</li>
+      <li 
+        v-for="(pannel,index) in pannels" 
+        @click="changeActive(index)"
+        :class="{'active': index === activeIndex}">{{ pannel }}</li>
     </ul>
     <ul class="emoji-container">
       <li 
@@ -10,12 +12,14 @@
         style="padding: 0" 
         :key="index"
         v-if="index === activeIndex">
-        <a href="javascript:;" v-for="emoji in emojiGroup" @click="selectItem(emoji)">
-           <span class = 'emoji-item'
-             :title = 'emoji'
-             :class = '"sprite-" + getPureName(emoji)'
-           >
-           </span>
+        <a 
+          href="javascript:;" 
+          v-for="(emoji, index) in emojiGroup"  
+          :key="index" @click="selectItem(emoji)">
+           <span 
+              class="emoji-item"
+              :title="emoji"
+              :class="'sprite-' + getPureName(emoji)"></span>
         </a>
       </li>
     </ul>
